0d8a8e0666fa386965ad062c0a0e54693c1f0f31
Projectgr7 <77547185+Projectgr7@users.noreply.github.com>
2021-01-17 15:42:50 +0300

    Add files via upload

---
+++ LeapYear.py
@@ -0,0 +1,12 @@
+# This project is implemented by (Amal, Alma, Suhaila) 7th grade at Tulip school
+year = int(input("enter a year : "))  
+if (year % 4) == 0:  
+   if (year % 100) == 0:  
+       if (year % 400) == 0:  
+           print("{0} is a leap year".format(year))  
+       else:  
+           print("{0} is not a leap year".format(year))  
+   else:  
+       print("{0} is a leap year".format(year))  
+else:  
+   print("{0} is not a leap year".format(year))
